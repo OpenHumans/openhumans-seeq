e89d00983ec90a3ce07b2073eba98d4e69a96bc7
Madeleine Ball <mpball@gmail.com>
2016-12-18 20:56:45 -0500

    Add celery and sqlalchemy

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn main:app --log-file=-
+worker: celery -A main.celery worker --without-gossip --without-mingle --without-heartbeat
--- main.py
+++ main.py
@@ -1,36 +1,147 @@
+from datetime import timedelta
 import os
+import time
 
-from flask import Flask, render_template, request
+import arrow
+from celery import Celery
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import requests
 import seeq
 
-CLIENT_ID = os.getenv('OH_CLIENT_ID', '')
-CLIENT_SECRET = os.getenv('OH_CLIENT_SECRET', '')
+# Celery settings
+CELERY_BROKER_URL = os.getenv('CLOUDAMQP_URL', 'amqp://')
+
+# Open Humans settings
+OH_CLIENT_ID = os.getenv('OH_CLIENT_ID', '')
+OH_CLIENT_SECRET = os.getenv('OH_CLIENT_SECRET', '')
 OH_BASE_URL = 'https://www.openhumans.org/'
-OHSEEQ_BASE_URL = os.getenv('DATAXFER_BASE_URL', 'http://127.0.0.1:5000/')
+
+# SEEQ settings
+SEEQ_REFRESH_TOKEN = os.getenv('SEEQ_REFRESH_TOKEN')
+SEEQ_API_KEY_PRODUCTION = os.getenv('SEEQ_API_KEY_PRODUCTION')
+SEEQ_STUDY_ID = int(os.getenv('SEEQ_STUDY_ID'))
+
+# Project details
+OHSEEQ_BASE_URL = os.getenv('OHSEEQ_BASE_URL', 'http://127.0.0.1:5000/')
 OH_PROJ_PAGE = 'https://www.openhumans.org/activity/seeq/'
-SEEQ_REFRESH_TOKEN=os.getenv('SEEQ_REFRESH_TOKEN')
-SEEQ_API_KEY_PRODUCTION=os.getenv('SEEQ_API_KEY_PRODUCTION')
-SEEQ_STUDY_ID=int(os.getenv('SEEQ_STUDY_ID'))
 
 # Default to DEBUG as True.
 DEBUG = False if os.getenv('DEBUG', '').lower() == 'False' else True
 
+# Set up Flask app.
 app = Flask(__name__)
+app.config.update(
+    DEBUG=DEBUG,
+    SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False)
 
-# Set up Flask app to log to stderr.
-import logging
-from logging import StreamHandler
-file_handler = StreamHandler()
-if DEBUG:
-    app.logger.setLevel(logging.DEBUG)
-else:
-    app.logger.setLevel(logging.INFO)
-app.logger.addHandler(file_handler)
+# Set up database.
+db = SQLAlchemy(app)
 
 
-def oh_exchange_code(code):
-    if CLIENT_SECRET and CLIENT_ID and code:
+class OpenHumansMember(db.Model):
+    """
+    Store OAuth2 data for Open Humans member.
+    """
+    id = db.Column(db.String, primary_key=True, unique=True)
+    access_token = db.Column(db.String)
+    refresh_token = db.Column(db.String)
+    token_expires = db.Column(db.String)
+
+    def __init__(self, id, access_token, refresh_token, expires_in):
+        self.id = id
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+        self.token_expires = (
+            arrow.now() + timedelta(seconds=expires_in)).format()
+
+    def __repr__(self):
+        return "<OpenHumansMember(id='{}')>".format(self.id)
+
+    def get_access_token(self):
+        """
+        Return access token. Refresh first if necessary.
+        """
+        # Also refresh if nearly expired (less than 60s remaining).
+        delta = timedelta(seconds=60)
+        if arrow.get(self.token_expires) - delta < arrow.now():
+            self._refresh_tokens()
+        return self.access_token
+
+    def _refresh_tokens(self):
+        """
+        Refresh access token.
+        """
+        response = requests.post(
+            'https://www.openhumans.org/oauth2/token/',
+            data={
+                'grant_type': 'refresh_token',
+                'refresh_token': self.refresh_token},
+            auth=requests.auth.HTTPBasicAuth(
+                OH_CLIENT_ID, OH_CLIENT_SECRET))
+        if response.status_code == 200:
+            data = response.json()
+            self.access_token = data['access_token']
+            self.refresh_token = data['refresh_token']
+            self.token_expires = (
+                arrow.now() + timedelta(seconds=data['expires_in'])).format()
+            db.session.commit()
+
+
+# Set up Celery with Heroku CloudAMQP (or AMQP in local dev).
+celery = Celery(__name__)
+celery.conf.update({
+    'BROKER_URL': CELERY_BROKER_URL,
+    # Recommended settings. See: https://www.cloudamqp.com/docs/celery.html
+    'BROKER_POOL_LIMIT': 1,
+    'BROKER_HEARTBEAT': None,
+    'BROKER_CONNECTION_TIMEOUT': 30,
+    'CELERY_RESULT_BACKEND': None,
+    'CELERY_SEND_EVENTS': False,
+    'CELERY_EVENT_QUEUE_EXPIRES': 60,
+})
+
+
+@celery.task
+def copy_data_to_open_humans(oh_id):
+    """
+    Copy data to Open Humans.
+    """
+    app.logger.info('Placeholder task: copy data for {} to Open Humans'.format(
+        oh_id))
+    time.sleep(10)
+    oh_member = OpenHumansMember.query.filter_by(id=oh_id).one()
+    token = oh_member.get_access_token()
+    app.logger.info('Token is: {}'.format(token))
+    oh_member._refresh_tokens()
+    token = oh_member.get_access_token()
+    app.logger.info('Token is: {}'.format(token))
+    oh_member2 = OpenHumansMember.query.filter_by(id=oh_id).one()
+    token = oh_member2.get_access_token()
+    app.logger.info('Token is: {}'.format(token))
+
+
+def oh_get_member_data(token):
+    """
+    Exchange OAuth2 token for member data.
+    """
+    req = requests.get(
+        '{}api/direct-sharing/project/exchange-member/'.format(OH_BASE_URL),
+        params={'access_token': token})
+    if req.status_code == 200:
+        return req.json()
+    raise Exception('Status code {}'.format(req.status_code))
+    return None
+
+
+def oh_code_to_member(code):
+    """
+    Exchange code for token, use this to create and return OpenHumansMember.
+
+    If a matching OpenHumansMember already exists in db, update and return it.
+    """
+    if OH_CLIENT_SECRET and OH_CLIENT_ID and code:
         data = {
             'grant_type': 'authorization_code',
             'redirect_uri': '{}complete'.format(OHSEEQ_BASE_URL),
@@ -40,50 +151,64 @@ def oh_exchange_code(code):
             '{}oauth2/token/'.format(OH_BASE_URL),
             data=data,
             auth=requests.auth.HTTPBasicAuth(
-                CLIENT_ID,
-                CLIENT_SECRET
+                OH_CLIENT_ID,
+                OH_CLIENT_SECRET
             ))
-        if 'access_token' in req.json():
-            return req.json()['access_token']
+        data = req.json()
+        if 'access_token' in data:
+            oh_id = oh_get_member_data(
+                data['access_token'])['project_member_id']
+            oh_member = OpenHumansMember.query.filter_by(
+                id=oh_id).one_or_none()
+            if not oh_member:
+                oh_member = OpenHumansMember(
+                    id=oh_id,
+                    access_token=data['access_token'],
+                    refresh_token=data['refresh_token'],
+                    expires_in=data['expires_in'])
+                db.session.add(oh_member)
+                app.logger.info('Member {} added.'.format(oh_id))
+            else:
+                app.logger.info('Member {} re-authorized.'.format(oh_id))
+                oh_member.access_token = data['access_token']
+                oh_member.refresh_token = data['refresh_token']
+                oh_member.token_expires = (arrow.now() + timedelta(
+                    seconds=data['expires_in'])).format()
+            db.session.commit()
+            return oh_member
         elif 'error' in req.json():
             app.logger.warning('Error in token exchange: {}'.format(req.json()))
         else:
             app.logger.warning('Neither token nor error info in OH response!')
     else:
-        app.logger.warning('CLIENT_SECRET or code are unavailable')
-    return ''
-
-
-def oh_exchange_token_for_id(token, logger=None):
-    """
-    Exchange OAuth2 token for member data, return project member ID.
-    """
-    req = requests.get(
-        'https://www.openhumans.org/api/direct-sharing/project/exchange-member/',
-        params={'access_token': token})
-    oh_id = req.json()['project_member_id']
-    if logger:
-        logger.debug("Token '{}' exchanged for project member ID '{}'".format(
-            token, oh_id))
-    return oh_id
+        app.logger.warning('OH_CLIENT_SECRET or code are unavailable')
+    return None
 
 
 @app.route("/")
 def index():
-    app.logger.debug("Loading index.html")
+    """
+    Starting page for app.
+    """
     return render_template(
-        'index.html', client_id=CLIENT_ID, oh_proj_page=OH_PROJ_PAGE)
+        'index.html', client_id=OH_CLIENT_ID, oh_proj_page=OH_PROJ_PAGE)
 
 
 @app.route("/complete", methods=['GET'])
 def complete():
-    app.logger.debug("Loading complete.html")
+    """
+    Receive user from Open Humans. Store data, start task, prompt Seeq auth.
+    """
+    app.logger.debug("Received user returning from Open Humans.")
     code = request.args.get('code', '')
-    token = oh_exchange_code(code=code)
-    oh_id = oh_exchange_token_for_id(token=token)
-    seeq_cli = seeq.client.Client(None)
-    seeq_url = seeq.util.jwt_signed(
-        SEEQ_STUDY_ID,
-        oh_id,
-        SEEQ_API_KEY_PRODUCTION)
-    return render_template('complete.html', token=token, oh_id=oh_id, seeq_url=seeq_url)
+    oh_member = oh_code_to_member(code=code)
+    if oh_member:
+        copy_data_to_open_humans.delay(oh_member.id)
+        seeq_url = seeq.util.jwt_signed(
+            SEEQ_STUDY_ID,
+            oh_member.id,
+            SEEQ_API_KEY_PRODUCTION)
+        return render_template(
+            'complete.html', oh_id=oh_member.id, seeq_url=seeq_url)
+    app.logger.info('Invalid code exchange. User returned to starting page.')
+    return redirect('/')
