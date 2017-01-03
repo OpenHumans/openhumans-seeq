3a51e87cd660acede6f6cfa21b025b6ff762efc1
Madeleine Ball <mpball@gmail.com>
2017-01-03 16:10:43 -0500

    Move tempdir creation/clean-up to within task

--- openhumans_seeq/dataxfer.py
+++ openhumans_seeq/dataxfer.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 import hashlib
 import json
 import os
-import tempfile
 try:
     import urlparse
 except ImportError:
@@ -79,10 +78,6 @@ def seeq_file_to_oh(oh_member, seeq_data, tempdir):
     another.
     """
     seeq_filename = urlparse.urlsplit(seeq_data['url_s3'])[2].split('/')[-1]
-    if not os.path.isdir(tempdir):
-        print("tempdir does not exist - generating new dir")
-        tempdir = tempfile.mkdtemp()
-        print("New tempdir: {}".format(tempdir))
     target_filepath = os.path.join(tempdir, seeq_filename)
     response = requests.get(seeq_data['url_s3'], stream=True)
     size = int(response.headers['Content-Length'])
